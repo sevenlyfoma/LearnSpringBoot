1ea3c49b0113683eb1327c7925962df657369c39
7lyfoma <7lyfoma@gmail.com>
2026-01-28 12:45:56 +0000

    continued with react tutorial

--- react/demo1/src/JsxDemo.jsx
+++ react/demo1/src/JsxDemo.jsx
@@ -1,4 +1,8 @@
-//https://www.w3schools.com/REACT/react_jsx_expressions.asp next page
+//https://www.w3schools.com/REACT/react_jsx_attributes.asp
+
+function MultByFive(num) {
+    return num * 5
+}
 
 function JsxDemo() {
     //Curly braces are computed
@@ -14,11 +18,36 @@ function JsxDemo() {
         </ul>
     );
 
+    const hp = 218 * 1.36;
+
+    const varExample = (
+        <p>My car has {hp} horsepower found from a variable</p>
+    );
+
+    const funcCallExample = (
+        <p>The result of 4*5 from a function call is {MultByFive(4)}</p>
+    )
+
+    const myobj = {
+        name: "Fiat",
+        model: "500",
+        color: "white"
+     };
+
+    const objectAttributesExample = (
+        <p>My car is a {myobj.color} {myobj.name} {myobj.model} these info gotten from objects fields</p>
+    );
+
+
     //Nest jsx elements with curly braces
     const combi = (
         <div>
             {myElement}
             {myListEle}
+            {varExample}
+            {funcCallExample}
+            {objectAttributesExample}
+            
         </div>
     )
 
