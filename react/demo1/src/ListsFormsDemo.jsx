2d1c7952dd4586d148e5332f1a338ffd305475cb
7lyfoma <7lyfoma@gmail.com>
2026-02-16 13:10:07 +0000

    add demo for forms controlled with hooks

--- react/demo1/src/ListsFormsDemo.jsx
+++ react/demo1/src/ListsFormsDemo.jsx
@@ -18,11 +18,33 @@ function ListDemo() {
   );
 }
 
+function MyForm() {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <p>Current value: {name}</p>
+    </form>
+  )
+}
+
 
 function ListsFormsDemo() {
      return (
             <div>
                  <ListDemo/>
+                 <MyForm/>
             </div>
         )
 }
