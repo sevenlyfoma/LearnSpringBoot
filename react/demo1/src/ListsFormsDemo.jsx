308da32aac5593c50b99e1966731b748704a3ac0
7lyfoma <7lyfoma@gmail.com>
2026-02-17 14:53:48 +0000

    added form submission demo

--- react/demo1/src/ListsFormsDemo.jsx
+++ react/demo1/src/ListsFormsDemo.jsx
@@ -39,12 +39,38 @@ function MyForm() {
   )
 }
 
+function MyFormSubmit() {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(name);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
+}
 
 function ListsFormsDemo() {
      return (
             <div>
                  <ListDemo/>
                  <MyForm/>
+                 <MyFormSubmit/>
             </div>
         )
 }
